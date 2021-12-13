ab68d646e14cf30af3d44589ec6dd6d9a2ad07a3
Kevin-Kroeger <kevin.kroeger@hebk.de>
2021-12-13 09:25:36 +0100

    RPG

--- RPG/TextRPG/src/main.java
+++ RPG/TextRPG/src/main.java
@@ -1,5 +1,30 @@
-public class main {
+
+import java.util.Scanner;
+
+import javax.security.auth.kerberos.KerberosCredMessage;
+public class Main {
     public static void main(String[] args) throws Exception {
+
+
+
+
+        // Scanner 
+        Scanner in = new Scanner(System.in);
+
+
         
+        System.out.println("--------------------------------------");
+        System.out.println("Wie willst du heißen:");
+
+
+        // Scanner reads the input and prints it 
+        String input = in.nextLine();
+        System.out.println("--------------------------------------");
+
+
+
+        System.out.println("Dein Name ist:");
+        System.out.println(input);
+    
     }
 }
